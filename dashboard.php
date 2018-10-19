2bd8472516c58169c9b35b5b30a6fd4e236b01b6
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-19 06:28:13 +0100

    added dev output via GET var

--- dashboard.php
+++ dashboard.php
@@ -290,8 +290,11 @@ desired effect
                 </section>
     
 				<section class="content">
-					<pre>
-					<?php print_r($site['miners']); ?>
+					<?php if(isset($_GET['dev'])){ ?>
+						<pre>
+						<?php print_r($site['miners']); ?>
+						</pre>
+					<?php } ?>
 					<div class="row">
 						<div class="col-md-12 col-xs-12">
 							<div class="col-md-3 col-xs-6">
