4338975a40fcc34516fdad63660e3e3c43b0a958
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-19 04:49:48 +0100

    no message

--- update.sh
+++ update.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-## miningcontrolpanel.com dashboard - update script (git pull)
+## miningcontrolpanel.com summary - update script (git pull)
 
-cd /home2/mcp/public_html/dashboard && git --git-dir=/home2/mcp/public_html/dashboard/.git pull origin master
-
-crontab /home2/mcp/public_html/dashboard/crontab.txt
+cd /home2/mcp/public_html/summary && git --git-dir=/home2/mcp/public_html/summary/.git pull origin master
 
 ## chmod 777 global_vars.php
