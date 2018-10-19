a0f6246a3f171cbe2c836c669e7c796449918f17
Jamie Whittingham <jamie.whittingham@gmail.com>
2018-10-19 06:24:46 +0100

    no message

--- dashboard.php
+++ dashboard.php
@@ -289,6 +289,7 @@ desired effect
                 </section>
     
 				<section class="content">
+					<pre>
 					<?php print_r($site['miners']); ?>
 					<div class="row">
 						<div class="col-md-12 col-xs-12">
